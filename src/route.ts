7b35be616acc7b8ff503fb9002d59e8a7c49490b
Nicolas Gere <nicolas@Nicolass-MacBook-Pro.local>
2024-02-07 09:16:30 -0800

    test

--- src/route.ts
+++ src/route.ts
@@ -4,7 +4,14 @@ import server from "./server";
 export const PingPayloadSchema = Type.Object({
   name: Type.String(),
 });
+
+export const PingResponseSchema = Type.Object({
+  name: Type.String(),
+});
+
 export type PingPayload = Static<typeof PingPayloadSchema>;
+export type PingReponse = Static<typeof PingResponseSchema>;
+
 export function registerRoute() {
   server.get("/", async (request, reply) => {
     const userRepositoryForReq = request.diScope.resolve("userService");
@@ -15,10 +22,13 @@ export function registerRoute() {
     {
       schema: {
         body: PingPayloadSchema,
+        response: {
+          201: PingResponseSchema
+        }
       },
     },
     async (request, reply) => {
-      return `pong ${request.body.name}`;
+      return reply.status(201).send({name: "Hello " + request.body.name});
     }
   );
 }
---
+++ src/utils.ts
@@ -0,0 +1,12 @@
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import { ContextConfigDefault, FastifyReply, FastifySchema, RawReplyDefaultExpression, RawRequestDefaultExpression, RawServerDefault, RouteGenericInterface } from "fastify";
+
+export type FastifyReplyTypebox<TSchema extends FastifySchema> = FastifyReply<
+  RawServerDefault,
+  RawRequestDefaultExpression,
+  RawReplyDefaultExpression,
+  RouteGenericInterface,
+  ContextConfigDefault,
+  TSchema,
+  TypeBoxTypeProvider
+>
